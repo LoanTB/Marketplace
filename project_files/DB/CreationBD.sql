8ee332da502472a593a57286ddcda0193aa933de
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2023-10-12 14:24:38 +0200

    Mise à jour de la base de données : Admin en héritage qui peut ban des clients et certifier des articles, et ajout de types de produits

--- project_files/DB/CreationBD.sql
+++ project_files/DB/CreationBD.sql
@@ -13,19 +13,35 @@ CREATE TABLE Utilisateur(
                             id_utilisateur INT AUTO_INCREMENT,
                             login VARCHAR(50)  NOT NULL,
                             email VARCHAR(100) ,
-                            telephone CHAR(11) ,
                             password CHAR(64)  NOT NULL,
-                            nom VARCHAR(50) ,
-                            prenom VARCHAR(50) ,
-                            nonce_email CHAR(20) ,
-                            nonce_telephone CHAR(20) ,
-                            admin BOOLEAN NOT NULL,
-                            id_image INT NOT NULL,
                             PRIMARY KEY(id_utilisateur),
                             UNIQUE(login),
-                            UNIQUE(email),
-                            UNIQUE(telephone),
-                            FOREIGN KEY(id_image) REFERENCES Image(id_image)
+                            UNIQUE(email)
+);
+
+CREATE TABLE Admin(
+                      id_utilisateur INT,
+                      PRIMARY KEY(id_utilisateur),
+                      FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur)
+);
+
+CREATE TABLE Type(
+                     nom_type VARCHAR(50) ,
+                     PRIMARY KEY(nom_type)
+);
+
+CREATE TABLE Client(
+                       id_utilisateur INT,
+                       telephone CHAR(11) ,
+                       nom VARCHAR(50) ,
+                       prenom VARCHAR(50) ,
+                       nonce_email CHAR(20) ,
+                       nonce_telephone CHAR(20) ,
+                       id_image INT NOT NULL,
+                       PRIMARY KEY(id_utilisateur),
+                       UNIQUE(telephone),
+                       FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur),
+                       FOREIGN KEY(id_image) REFERENCES Image(id_image)
 );
 
 CREATE TABLE Article(
@@ -35,8 +51,10 @@ CREATE TABLE Article(
                         description VARCHAR(500) ,
                         quantite INT NOT NULL,
                         id_utilisateur INT NOT NULL,
+                        id_utilisateur_1 INT NOT NULL,
                         PRIMARY KEY(id_article),
-                        FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur)
+                        FOREIGN KEY(id_utilisateur) REFERENCES Admin(id_utilisateur),
+                        FOREIGN KEY(id_utilisateur_1) REFERENCES Client(id_utilisateur)
 );
 
 CREATE TABLE Commenter(
@@ -46,7 +64,7 @@ CREATE TABLE Commenter(
                           texte VARCHAR(500)  NOT NULL,
                           note DOUBLE NOT NULL,
                           PRIMARY KEY(id_utilisateur, id_article),
-                          FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur),
+                          FOREIGN KEY(id_utilisateur) REFERENCES Client(id_utilisateur),
                           FOREIGN KEY(id_article) REFERENCES Article(id_article)
 );
 
@@ -62,7 +80,7 @@ CREATE TABLE posseder(
                          id_utilisateur INT,
                          id_wishlist INT,
                          PRIMARY KEY(id_utilisateur, id_wishlist),
-                         FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur),
+                         FOREIGN KEY(id_utilisateur) REFERENCES Client(id_utilisateur),
                          FOREIGN KEY(id_wishlist) REFERENCES Wishlist(id_wishlist)
 );
 
@@ -72,4 +90,22 @@ CREATE TABLE souhaiter(
                           PRIMARY KEY(id_article, id_wishlist),
                           FOREIGN KEY(id_article) REFERENCES Article(id_article),
                           FOREIGN KEY(id_wishlist) REFERENCES Wishlist(id_wishlist)
+);
+
+CREATE TABLE ban(
+                    id_utilisateur INT,
+                    id_utilisateur_1 INT,
+                    motif VARCHAR(200) ,
+                    fin DATE,
+                    PRIMARY KEY(id_utilisateur, id_utilisateur_1),
+                    FOREIGN KEY(id_utilisateur) REFERENCES Client(id_utilisateur),
+                    FOREIGN KEY(id_utilisateur_1) REFERENCES Admin(id_utilisateur)
+);
+
+CREATE TABLE type_produit(
+                             id_article INT,
+                             nom_type VARCHAR(50) ,
+                             PRIMARY KEY(id_article, nom_type),
+                             FOREIGN KEY(id_article) REFERENCES Article(id_article),
+                             FOREIGN KEY(nom_type) REFERENCES Type(nom_type)
 );
