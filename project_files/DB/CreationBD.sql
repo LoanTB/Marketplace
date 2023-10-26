e52b505a6b0fee94107ac2940e65a952acbdf4d2
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2023-10-26 11:01:16 +0200

    Mise à jour de la base de données

--- project_files/DB/CreationBD.sql
+++ project_files/DB/CreationBD.sql
@@ -1,111 +1,96 @@
 CREATE TABLE Image(
-                      id_image INT AUTO_INCREMENT,
-                      PRIMARY KEY(id_image)
+   url VARCHAR(100) ,
+   PRIMARY KEY(url)
 );
 
 CREATE TABLE Wishlist(
-                         id_wishlist INT AUTO_INCREMENT,
-                         nom VARCHAR(50)  NOT NULL,
-                         PRIMARY KEY(id_wishlist)
-);
-
-CREATE TABLE Compte(
-                       id_compte INT AUTO_INCREMENT,
-                       login VARCHAR(50)  NOT NULL,
-                       email VARCHAR(100) ,
-                       password CHAR(64)  NOT NULL,
-                       PRIMARY KEY(id_compte),
-                       UNIQUE(login),
-                       UNIQUE(email)
-);
-
-CREATE TABLE Admin(
-                      id_compte INT,
-                      PRIMARY KEY(id_compte),
-                      FOREIGN KEY(id_compte) REFERENCES Compte(id_compte)
+   id_wishlist INT AUTO_INCREMENT,
+   nom VARCHAR(50)  NOT NULL,
+   PRIMARY KEY(id_wishlist)
 );
 
 CREATE TABLE Type(
-                     nom_type VARCHAR(50) ,
-                     PRIMARY KEY(nom_type)
+   nom_type VARCHAR(50) ,
+   PRIMARY KEY(nom_type)
 );
 
 CREATE TABLE Utilisateur(
-                            id_compte INT,
-                            telephone CHAR(11) ,
-                            nom VARCHAR(50) ,
-                            prenom VARCHAR(50) ,
-                            nonce_email CHAR(20) ,
-                            nonce_telephone CHAR(20) ,
-                            id_image INT NOT NULL,
-                            PRIMARY KEY(id_compte),
-                            UNIQUE(telephone),
-                            FOREIGN KEY(id_compte) REFERENCES Compte(id_compte),
-                            FOREIGN KEY(id_image) REFERENCES Image(id_image)
+   id_compte INT AUTO_INCREMENT,
+   login VARCHAR(50)  NOT NULL,
+   email VARCHAR(100) ,
+   telephone CHAR(11) ,
+   password CHAR(64)  NOT NULL,
+   nom VARCHAR(50) ,
+   prenom VARCHAR(50) ,
+   nonce_email CHAR(20) ,
+   nonce_telephone CHAR(20) ,
+   admin BOOLEAN NOT NULL,
+   url VARCHAR(100)  NOT NULL,
+   PRIMARY KEY(id_compte),
+   UNIQUE(login),
+   UNIQUE(email),
+   UNIQUE(telephone),
+   FOREIGN KEY(url) REFERENCES Image(url)
 );
 
 CREATE TABLE Article(
-                        id_article INT AUTO_INCREMENT,
-                        nom VARCHAR(50)  NOT NULL,
-                        description VARCHAR(500) ,
-                        prix DECIMAL(15,2)   NOT NULL,
-                        quantite INT NOT NULL,
-                        id_compte INT NOT NULL,
-                        id_compte_1 INT NOT NULL,
-                        PRIMARY KEY(id_article),
-                        FOREIGN KEY(id_compte) REFERENCES Admin(id_compte),
-                        FOREIGN KEY(id_compte_1) REFERENCES Utilisateur(id_compte)
+   id_article INT AUTO_INCREMENT,
+   nom VARCHAR(50)  NOT NULL,
+   description VARCHAR(500) ,
+   prix DECIMAL(15,2)   NOT NULL,
+   quantite INT NOT NULL,
+   id_compte INT NOT NULL,
+   PRIMARY KEY(id_article),
+   FOREIGN KEY(id_compte) REFERENCES Utilisateur(id_compte)
 );
 
 CREATE TABLE Commenter(
-                          id_compte INT,
-                          id_article INT,
-                          titre VARCHAR(50)  NOT NULL,
-                          texte VARCHAR(500)  NOT NULL,
-                          note DOUBLE NOT NULL,
-                          PRIMARY KEY(id_compte, id_article),
-                          FOREIGN KEY(id_compte) REFERENCES Utilisateur(id_compte),
-                          FOREIGN KEY(id_article) REFERENCES Article(id_article)
+   id_compte INT,
+   id_article INT,
+   titre VARCHAR(50)  NOT NULL,
+   texte VARCHAR(500)  NOT NULL,
+   note DOUBLE NOT NULL,
+   PRIMARY KEY(id_compte, id_article),
+   FOREIGN KEY(id_compte) REFERENCES Utilisateur(id_compte),
+   FOREIGN KEY(id_article) REFERENCES Article(id_article)
 );
 
 CREATE TABLE illustrer(
-                          id_article INT,
-                          id_image INT,
-                          PRIMARY KEY(id_article, id_image),
-                          FOREIGN KEY(id_article) REFERENCES Article(id_article),
-                          FOREIGN KEY(id_image) REFERENCES Image(id_image)
+   id_article INT,
+   url VARCHAR(100) ,
+   PRIMARY KEY(id_article, url),
+   FOREIGN KEY(id_article) REFERENCES Article(id_article),
+   FOREIGN KEY(url) REFERENCES Image(url)
 );
 
 CREATE TABLE posseder(
-                         id_compte INT,
-                         id_wishlist INT,
-                         PRIMARY KEY(id_compte, id_wishlist),
-                         FOREIGN KEY(id_compte) REFERENCES Utilisateur(id_compte),
-                         FOREIGN KEY(id_wishlist) REFERENCES Wishlist(id_wishlist)
+   id_compte INT,
+   id_wishlist INT,
+   PRIMARY KEY(id_compte, id_wishlist),
+   FOREIGN KEY(id_compte) REFERENCES Utilisateur(id_compte),
+   FOREIGN KEY(id_wishlist) REFERENCES Wishlist(id_wishlist)
 );
 
 CREATE TABLE souhaiter(
-                          id_article INT,
-                          id_wishlist INT,
-                          PRIMARY KEY(id_article, id_wishlist),
-                          FOREIGN KEY(id_article) REFERENCES Article(id_article),
-                          FOREIGN KEY(id_wishlist) REFERENCES Wishlist(id_wishlist)
-);
-
-CREATE TABLE ban(
-                    id_compte INT,
-                    id_compte_1 INT,
-                    motif VARCHAR(200) ,
-                    fin DATE,
-                    PRIMARY KEY(id_compte, id_compte_1),
-                    FOREIGN KEY(id_compte) REFERENCES Utilisateur(id_compte),
-                    FOREIGN KEY(id_compte_1) REFERENCES Admin(id_compte)
+   id_article INT,
+   id_wishlist INT,
+   PRIMARY KEY(id_article, id_wishlist),
+   FOREIGN KEY(id_article) REFERENCES Article(id_article),
+   FOREIGN KEY(id_wishlist) REFERENCES Wishlist(id_wishlist)
 );
 
 CREATE TABLE type_produit(
-                             id_article INT,
-                             nom_type VARCHAR(50) ,
-                             PRIMARY KEY(id_article, nom_type),
-                             FOREIGN KEY(id_article) REFERENCES Article(id_article),
-                             FOREIGN KEY(nom_type) REFERENCES Type(nom_type)
+   id_article INT,
+   nom_type VARCHAR(50) ,
+   PRIMARY KEY(id_article, nom_type),
+   FOREIGN KEY(id_article) REFERENCES Article(id_article),
+   FOREIGN KEY(nom_type) REFERENCES Type(nom_type)
+);
+
+CREATE TABLE dansPanier(
+   id_compte INT,
+   id_article INT,
+   PRIMARY KEY(id_compte, id_article),
+   FOREIGN KEY(id_compte) REFERENCES Utilisateur(id_compte),
+   FOREIGN KEY(id_article) REFERENCES Article(id_article)
 );
