444e3570e87664850b004c5ede75f9a5b291fd39
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2023-12-02 17:30:24 +0100

    fix: supprimerDuPanier problèmes de droits et de suppression

--- hosting/src/Controleur/ControleurPanier.php
+++ hosting/src/Controleur/ControleurPanier.php
@@ -55,12 +55,12 @@ class ControleurPanier extends ControleurGenerique {
             return;
         }
 
-        if ((new dansPanierRepository())->recupererParColonne($_REQUEST["id_article"],1) == null) {
+        if ((new dansPanierRepository())->recupererParDeuxColonne(ConnexionUtilisateur::getIdUtilisateurConnecte(),0,$_REQUEST["id_article"],1) == null) {
             ControleurGenerique::accesNonAutorise();
             return;
         }
 
-        $sqlreturn = (new dansPanierRepository())->supprimerParColonne($_REQUEST["id_article"],1);
+        $sqlreturn = (new dansPanierRepository())->supprimerParDeuxColonne(ConnexionUtilisateur::getIdUtilisateurConnecte(),0,$_REQUEST["id_article"],1);
 
         if ($sqlreturn == "") {
             MessageFlash::ajouter("success", "L'article a bien été supprimé du panier.");
