f3f7bd2624b28a0693fecda1ece3553f634e210e
Cordeilla Lucas <lucas.cordeilla@etu.umontpellier.fr>
2023-12-03 20:27:25 +0100

    feat(ControleurArticle): implémentation miseAJour

--- hosting/src/Controleur/ControleurArticle.php
+++ hosting/src/Controleur/ControleurArticle.php
@@ -5,6 +5,7 @@ use App\Ecommerce\Lib\ConnexionUtilisateur;
 use App\Ecommerce\Lib\MessageFlash;
 use App\Ecommerce\Modele\DataObject\Article;
 use App\Ecommerce\Modele\DataObject\Image;
+use App\Ecommerce\Modele\DataObject\relations\dansPanier;
 use App\Ecommerce\Modele\DataObject\relations\illustrer;
 use App\Ecommerce\Modele\Repository\ArticleRepository;
 use App\Ecommerce\Lib\ImgurUploader;
@@ -170,14 +171,73 @@ class ControleurArticle extends ControleurGenerique {
 
         $article = new Article($_REQUEST["id_article"],$_REQUEST["nom"],$_REQUEST["description"],$_REQUEST["prix"],$_REQUEST["quantite"],(new DateTime())->format('Y-m-d H:i:s'),(new DateTime())->format('Y-m-d H:i:s'),ConnexionUtilisateur::getIdUtilisateurConnecte(),$raw = false);
 
+        $uploader = new ImgurUploader();
+        $imgurLinks = array();
+
+        for ($i = 1; $i <= 3; $i++) {
+            $inputName = 'image' . $i;
+            if ($_FILES[$inputName] && $_FILES[$inputName]['error'] === UPLOAD_ERR_OK) {
+                $fileToUpload = array('image' => $_FILES[$inputName]);
+                $imgurLink = $uploader->uploadImage($fileToUpload);
+                if ($imgurLink) {
+                    $imgurLinks[] = $imgurLink;
+                } else {
+                    MessageFlash::ajouter("warning", "Impossible d'héberger les images, veuillez réessayer ultérieurement");
+                    self::afficherFormulaireCreation();
+                    return;
+                }
+            }
+        }
+
+        $i = 0;
+        $newIllustrations = [];
+        foreach ($imgurLinks as $link) {
+            $image = new Image($link);
+            $sqlreturn = (new ImageRepository())->ajouter($image);
+            if ($sqlreturn != "") {
+                MessageFlash::ajouter("warning", "L'image n'a pas pu être sauvegardé dans la table image, veuillez réessayer plus tard.");
+                self::afficherListe();
+                return;
+            }
+            $newIllustrations[] = new illustrer($_REQUEST["id_article"], $link, $i);
+            $i ++;
+        }
+
+        $oldIllustrations = (new illustrerRepository())->recupererParColonne($_REQUEST["id_article"], 0);
+
+        foreach ($newIllustrations as $newIllustration) {
+            $remplacer = false;
+            foreach ($oldIllustrations as $oldIllustration) {
+                if ($oldIllustration->getOrdre() == $newIllustration->getOrdre()) {
+                    $sqlreturn = (new illustrerRepository())->supprimer($oldIllustration);
+                    if ($sqlreturn != "") {
+                        MessageFlash::ajouter("warning", "L'article n'as pas pu être mis à jour (s".$sqlreturn."), veuillez réessayer plus tard.");
+                        self::afficherListe();
+                        return;
+                    }
+                    $sqlreturn = (new illustrerRepository())->ajouter($newIllustration);
+                    if ($sqlreturn != "") {
+                        MessageFlash::ajouter("warning", "L'article n'as pas pu être mis à jour (a".$sqlreturn."), veuillez réessayer plus tard.");
+                        self::afficherListe();
+                        return;
+                    }
+                    $remplacer = true;
+                }
+            }
+            if (!$remplacer) {
+                $sqlreturn = (new illustrerRepository())->ajouter($newIllustration);
+                if ($sqlreturn != "") {
+                    MessageFlash::ajouter("warning", "L'article n'as pas pu être mis à jour (a".$sqlreturn."), veuillez réessayer plus tard.");
+                    self::afficherListe();
+                    return;
+                }
+            }
+        }
+
         $sqlreturn = (new ArticleRepository())->mettreAJour($article);
 
-        if ($sqlreturn == "22001"){
-            MessageFlash::ajouter("warning", "Une information de mauvaise taille à été entrée, veuillez la raccourcir.");
-            self::afficherFormulaireMiseAJour();
-            return;
-        } else if ($sqlreturn != "") {
-            MessageFlash::ajouter("warning", "L'article n'as pas pu être créé (".$sqlreturn."), veuillez réessayer plus tard.");
+        if ($sqlreturn != "") {
+            MessageFlash::ajouter("warning", "L'article n'as pas pu être mis à jour (m".$sqlreturn."), veuillez réessayer plus tard.");
             self::afficherListe();
             return;
         }
