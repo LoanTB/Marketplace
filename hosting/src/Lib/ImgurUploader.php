d32035f2bf37848c4284c1cb2d577a69c55d22bc
Cordeilla Lucas <lucas.cordeilla@etu.umontpellier.fr>
2023-12-03 13:50:48 +0100

    refactor(ImgurUploader): changement constructeur

--- hosting/src/Lib/ImgurUploader.php
+++ hosting/src/Lib/ImgurUploader.php
@@ -4,8 +4,8 @@ class ImgurUploader {
     private $clientId;
     private $apiUrl;
 
-    public function __construct($clientId) {
-        $this->clientId = $clientId;
+    public function __construct() {
+        $this->clientId = '4eb5daa9993a516';
         $this->apiUrl = 'https://api.imgur.com/3/image';
     }
 
