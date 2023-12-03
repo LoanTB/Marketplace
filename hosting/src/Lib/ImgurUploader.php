432d17e37a82de8b2ad7eae291f1f7106ba399d7
Cordeilla Lucas <lucas.cordeilla@etu.umontpellier.fr>
2023-12-03 13:47:33 +0100

    feat(ImgurUploader): ajout fonction upload qui renvois le liens associé à l'image

--- hosting/src/Lib/ImgurUploader.php
+++ hosting/src/Lib/ImgurUploader.php
@@ -8,5 +8,40 @@ class ImgurUploader {
         $this->clientId = $clientId;
         $this->apiUrl = 'https://api.imgur.com/3/image';
     }
+
+    public function uploadImage($file) {
+        if (isset($file['image']) && $file['image']['error'] === UPLOAD_ERR_OK) {
+            $imageData = file_get_contents($file['image']['tmp_name']);
+
+            $headers = array(
+                'Authorization: Client-ID ' . $this->clientId,
+            );
+
+            $postData = array(
+                'image' => base64_encode($imageData),
+            );
+
+            $options = array(
+                'http' => array(
+                    'header'  => implode("\r\n", $headers),
+                    'method'  => 'POST',
+                    'content' => http_build_query($postData),
+                ),
+            );
+
+            $context  = stream_context_create($options);
+            $response = file_get_contents($this->apiUrl, false, $context);
+
+            $jsonResponse = json_decode($response, true);
+
+            if ($jsonResponse && isset($jsonResponse['data']['link'])) {
+                return $jsonResponse['data']['link'];
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
 ?>
